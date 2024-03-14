82cce1c1265c1b015a79b87b2b30212ac8a60a79
William M <cominatyou@cominatyou.com>
2024-03-14 00:15:47 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import fs from "node:fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: react(),
-  server: {
-    host: "loopback.cominatyou.com",
-    port: 443,
-    https: {
-      key: fs.readFileSync("C:\\Users\\willi\\Documents\\dev\\loopback-certs\\loopback-key.pem"),
-      cert: fs.readFileSync("C:\\Users\\willi\\Documents\\dev\\loopback-certs\\fullchain.pem")
-    }
-  }
+  plugins: react()
 })
